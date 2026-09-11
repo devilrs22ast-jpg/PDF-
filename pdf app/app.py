333322a3675c874026eb86cbcbe49c2c4174b57c
devilrs22ast-jpg <devilrs22ast@gmail.com>
2026-09-11 09:17:41 +0530

    Create app.py

---
+++ pdf app/app.py
@@ -0,0 +1,4 @@
+st.sidebar.markdown("---")
+st.sidebar.write("If this tool helped you, consider supporting my work! ☕")
+# Ensure the image file name exactly matches what you uploaded
+st.sidebar.image("fampay.jpg", caption="Scan to pay via UPI")
